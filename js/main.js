b31a7868ffd1308a38618e5b1f6ba1490118a1be
Dragoteryx <33252782+Dragoteryx@users.noreply.github.com>
2018-04-26 14:50:50 +0200

    left menu scrolling fix

--- js/main.js
+++ js/main.js
@@ -4,28 +4,28 @@ function showFact() {
 function adjust() {
   $("#content").css("margin-left", $("#leftmenu").css("width"));
 }
+function setScrolling() {
+  let scrollTop = $(window).scrollTop();
+  let scrollBottom = scrollTop + $(window).height();
+  if (scrollTop == 0)
+    $("#upbutton").addClass("disabled");
+  else
+    $("#upbutton").removeClass("disabled");
+  if (scrollTop > ($("#menu").offset().top + 50)) {
+    $("#leftmenu").css("position", "fixed");
+    $("#leftmenu").css("top", "0");
+  } else {
+    $("#leftmenu").css("position", "absolute");
+    $("#leftmenu").css("top", "");
+  }
+}
 $(document).ready(function() {
   adjust();
   showFact();
+  setScrolling();
   $(window).resize(adjust);
   $("#showfact").click(showFact);
-  if ($(window).scrollTop() == 0)
-    $("#upbutton").addClass("disabled");
-  $(window).scroll(function() {
-    let scrollTop = $(window).scrollTop();
-    let scrollBottom = scrollTop + $(window).height();
-    if (scrollTop == 0)
-      $("#upbutton").addClass("disabled");
-    else
-      $("#upbutton").removeClass("disabled");
-    if (scrollTop > ($("#menu").offset().top + 50)) {
-      $("#leftmenu").css("position", "fixed");
-      $("#leftmenu").css("top", "0");
-    } else {
-      $("#leftmenu").css("position", "absolute");
-      $("#leftmenu").css("top", "");
-    }
-  });
+  $(window).scroll(setScrolling);
   $(".button, .cadre").mouseenter(function() {
     $(this).addClass("animated pulse");
   });

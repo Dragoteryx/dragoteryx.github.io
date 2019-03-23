406f49efe2e8362da7436e9eb64a7c474fa61ac0
Dragoteryx <33252782+Dragoteryx@users.noreply.github.com>
2019-03-24 00:34:03 +0100

    steam page link

--- angular/src/app/navmenu/navmenu.component.ts
+++ angular/src/app/navmenu/navmenu.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavmenuComponent implements OnInit {
   websites = [
     {name: "Twitter", href: "https://twitter.com/Dragoteryx"},
+    {name: "Steam", href: "https://steamcommunity.com/id/dragoteryx/"},
     {name: "Github", href: "https://github.com/Dragoteryx"}
   ];
   locals = [];
